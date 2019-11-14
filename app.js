0f17fccfe95ff0f4272015f400771b9194ac3bbe
Impius <impius3000@gmail.com>
2019-11-13 23:03:10 -0800

    Add button & route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 app.use(express.static('html'));
 
-app.get('/test', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('D&D Generator'));
+app.get('/beholder', (req, res) => res.send('Beauty is in the eye of the...'));
+app.get('/rumor', (req, res) => res.json({"name": "MisBod" , "description": "Bodies missing from graves"}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
